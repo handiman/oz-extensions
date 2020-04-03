90c4c16c30827239eb421d35dca1541d1dd23f94
Henrik <handiman@users.noreply.github.com>
2020-04-03 08:01:04 +0200

    Exceptions

---
+++ oz-extensions/ArgumentIsFuckAllException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace OzExtensions
+{
+    public class ArgumentIsFuckAllException : ArgumentNullException
+    {
+        public ArgumentIsFuckAllException() { }
+        public ArgumentIsFuckAllException(string paramName) : base(paramName) { }
+        public ArgumentIsFuckAllException(string paramName, string message) : base(paramName, message) { }
+        public ArgumentIsFuckAllException(string message, Exception exception) : base(message, exception) { }
+        public ArgumentIsFuckAllException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ oz-extensions/ArgumentIsFuckedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace OzExtensions
+{
+    public class ArgumentIsFuckedException : ArgumentException
+    {
+        public ArgumentIsFuckedException() { }
+        public ArgumentIsFuckedException(string message) : base(message) { }
+        public ArgumentIsFuckedException(string message, string paramName) : base(message, paramName) { }
+        public ArgumentIsFuckedException(string message, Exception exception) : base(message, exception) { }
+        public ArgumentIsFuckedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
